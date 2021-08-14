c77115961b43a75fb70d0516d16ae759f17a68ea
Raquel Uribe <raquel.uribe.sierra@gmail.com>
2021-08-14 14:59:35 -0500

    operators added

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ document.querySelector('#button-divide').addEventListener('click', () => {
 function sumar (a, b) {
     return a + b;
 }
-function restar (a,b) => a − b
-function multiplicar (a,b) => a × b
-function dividir (a, b) => a ÷ b
+function restar (a,b) => a - b
+function multiplicar (a,b) => a * b
+function dividir (a, b) => a / b
 
 
 // Traer todos los botones
@@ -32,6 +32,43 @@ for (let i = 0; i < allBtn.length; i++) {
     allBtn[i].addEventListener('click', (event) => {
         const element = event.target; // Elemento .calc-button que dispara el evento
     
-        console.log(`Clic en ${element.textContent}`);
+       if (element.textContent == 'C') {
+           logEl.innerText ='';
+           logArr.length = 0;
+           aux = '';
+           resultEl.value = 0;
+       } else if (element.textContet == '=') {
+           logArr.push(aux);
+       }
+       
+       let total = parseInt(logArr[0]);
+
+       // [1, +, 2, +, 3, +, 4, +, 5]
+            // #1 -> 1 + 2 = 3
+            // #2 -> 3 + 3 = 6
+            // #3 -> 6 + 4 = 10
+            // #4 -> 10 + 5 = 15
+
+            for(let i = 1; i < logArr.length; i+=2) {
+                const op = logArr[i];
+                const b = parseInt(logArr[i+1]);
+
+                //debugger;
+
+                if (op == '+') {
+                    total = sumar(total, b);
+                } else if (op == '−') {
+                    total = restar(total, b);
+                } else if (op == '×') {
+                    total = multiplicar(total, b);
+                } else if (op == '÷') {
+                    total = dividir(total, b);
+                }
+            }
+
+            console.log(total);
+            logArr.length = 0;
+            aux = '';
+            
     });
 }
